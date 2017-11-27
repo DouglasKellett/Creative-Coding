bdf4f0e96a19550698e59cfa00cb5ba046042b14
Doug Kellett <dug.kellett@gmail.com>
2017-11-27 14:21:07 +0000

    lk

--- DAT-GAD-405_2017-master/Session3/01_For-Loop/sketch.js
+++ DAT-GAD-405_2017-master/Session3/01_For-Loop/sketch.js
@@ -8,20 +8,20 @@
 let size = 50;
 
 function setup() {
-  createCanvas(500, 500);
-  noLoop();
+  createCanvas(1200, 600);
+  //noLoop();
+  frameRate(24)
+
 }
 
 function draw() {
   background(255, 0, 0);
-  fill(215, 200, 170);
-  //For-loop that will create 10 shapes on the window
-  for (let x = 0; x < 500; x+=50) {
-    //x position is adjusted by the variable x (+1 on each loop)
-    //Thus, x goes from 0 to 10. Multiplying by 50 it will create this sequence:
-    //0, 50, 100, 150, 200, 250, 300, 350, 400, 450
-    rect(x, 0, size, size);
-    rect(x, height/2 - size/2, size, size);
-    rect(x, height-size, size, size);
-  }
+  push()
+  fill(random(255));
+  //rotate(random(360));
+  for (let x = 0; x < 1000; x++) {
+    pop
+    rect(random(width), random(height), random(100), random(100));
+
+}
 }
