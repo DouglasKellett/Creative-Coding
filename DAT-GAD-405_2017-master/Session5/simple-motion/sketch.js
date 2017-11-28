00986f97faebe9da45f6c8efdcf6fd350424963a
Doug Kellett <dug.kellett@gmail.com>
2017-11-28 14:54:02 +0000

    28/11/2017

--- DAT-GAD-405_2017-master/Session5/simple-motion/sketch.js
+++ DAT-GAD-405_2017-master/Session5/simple-motion/sketch.js
@@ -1,10 +1,6 @@
-let x;
-let y;
-let speedX;
-let speedY;
-let r = 255;
-let g = 255;
-let b = 255;
+let x; let y;
+let speedX; let speedY;
+let r = 255; let g = 255; let b = 255;
 function setup() {
   createCanvas(480, 640);
   x = width/2;
@@ -15,19 +11,33 @@ function setup() {
 }
 function draw() {
   background(100);
-  x += speedX;
-  y += speedY;
+  x = x + speedX;
+  y = y + speedY;
   if (x>width) {
-     x = 0
+     speedX = speedX *-1.;
      r = random(255)
      g = random(255)
      b = random(255)
-
   }
-  if (y>height) y = 0;
-  if (x<0) x = width;
-  if (y<0) y = height;
-  fill(r, g, b);
+  if (y>height) {
+    speedY = speedY *-1.;
+    r = random(255)
+    g = random(255)
+    b = random(255)
+  }
+  if (x<0) {
+    x = speedX = speedX *-1.;
+    r = random(255)
+    g = random(255)
+    b = random(255)
+  }
+  if (y<0) {
+     y = speedY = speedY *-1.;
+     r = random(255)
+     g = random(255)
+     b = random(255)
+  }
+  fill(r, g, b, 120);
   ellipse(x, y, 50, 50);
   fill(255);
   text(x, width/2, height/2-20);
