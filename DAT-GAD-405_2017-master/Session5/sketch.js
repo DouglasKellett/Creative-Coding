9418cb2371a6b0980e6ad5ef6008f70f417f38f8
Doug Kellett <dug.kellett@gmail.com>
2017-11-28 09:30:17 +0000

    comit

--- DAT-GAD-405_2017-master/Session5/sketch.js
+++ DAT-GAD-405_2017-master/Session5/sketch.js
@@ -3,13 +3,21 @@ let speed = 3; let diameter = 50;
 let x1; let y1;
 function setup() {
   createCanvas(500, 500);
-  x1 = width
-
-
+  x1 = width/2;
+  y1 = height/2;
+  background(200);
 }
-
 function draw() {
-  background(255, 0, 0);
-
+  x1 += random(-speed, speed);
+  y1 += random(-speed, speed);
+  x1 = constrain(x1, 0 , width);
+  y1 = constrain(y1, 0, height);
+  if (mouseIPressed) {
+    r = random(255);
+    g = random(255);
+    b = random(255);
+  }
+  fill (r, g, b, 120);
+  elipse(x1, y1, diameter, diameter);
 }
 }
