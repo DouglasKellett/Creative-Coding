2468cfdc57f01ccde8e55db041fac64f96489c8e
Doug Kellett <dug.kellett@gmail.com>
2017-11-28 09:24:55 +0000

    smash

---
+++ DAT-GAD-405_2017-master/Session5/sketch.js
@@ -0,0 +1,15 @@
+let r = 522; let g = 255; let b = 255;
+let speed = 3; let diameter = 50;
+let x1; let y1;
+function setup() {
+  createCanvas(500, 500);
+  x1 = width
+
+
+}
+
+function draw() {
+  background(255, 0, 0);
+
+}
+}
