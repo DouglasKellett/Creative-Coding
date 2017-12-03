9f37f90d96ef8445c7adb462ec57dce84f42595f
Doug Kellett <dug.kellett@gmail.com>
2017-12-03 17:49:12 +0000

    03/12/2017

--- DAT-GAD-405_2017-master/Dynamic Emergencies/sketch.js
+++ DAT-GAD-405_2017-master/Dynamic Emergencies/sketch.js
@@ -1,8 +1,17 @@
 
 function setup() {
-  createCanvas(480, 640);
+  createCanvas(594, 841);
+  frameRate(10);
 }
 function draw() {
   background(100);
+
+  let x = random(400);
+  let y = random(400);
+
+  stroke(255);
+  line(x, x, x, y);
+
+
 
 }
--- DAT-GAD-405_2017-master/Session3/01_For-Loop/sketch.js
+++ DAT-GAD-405_2017-master/Session3/01_For-Loop/sketch.js
@@ -1,25 +1,33 @@
 let r = 255; let g = 255; let b = 255;
-let size = 50;
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(594, 841);
   //noLoop();
-  frameRate(24)
+  frameRate(24);
 
 }
 
 function draw() {
-  background(255, 0, 0);
+  background(0);
 
   r = random(255);
   g = random(255);
   b = random(255);
 
-  fill(r, g, b);
-  for (let x = 0; x < 800; x++) {
-    push()
-    rect(random(x), random(x), random(size), random(size));
-    pop()
+  let size = random(30);
+
+  stroke(255, 255, 0);
+  for (let x = 300; x < 900; x++) {
+    line(random(x), random(x), mouseX, mouseY);
   }
+
+  stroke(255);
+  for (let y = 300; y < 900; y++) {
+    line(random(y), random(y), mouseX, mouseY);
+  }
+  stroke(0)
+  fill(0);
+  ellipse(mouseX, mouseY, size, size);
+
 
 }
