d7a89079c4037a3f4b6cb1e6187ccbc072537d7b
DouglasKellett <dug.kellett@gmail.com>
2017-12-09 16:57:28 +0000

    09/12/2017

--- DAT-GAD-405_2017-master/Dynamic Emergencies/sketch.js
+++ DAT-GAD-405_2017-master/Dynamic Emergencies/sketch.js
@@ -6,20 +6,27 @@ function draw() {
   background(0);
   frameRate(24);
 
-
+//outline of the rects randomly change color every time the frame is updated
   stroke(random(255), random(255), random(255));
+//rects all have black fill
   fill(0);
+//loops that causes rects to appear in random places
+//rects randomly appear top left of mouse
   for (let x = 300; x < 1000; x++) {
+    //mouse position changes rect dimensions
     rect(mouseX - random(x), mouseY - random(x), mouseX - 300, mouseY - 420);
   }
+//rects randomly appear top right of mouse
   stroke(random(255), random(255), random(255))
   for (let x = 300; x < 1000; x++) {
     rect(mouseX + random(x), mouseY - random(x), mouseX - 300, mouseY - 420);
   }
+//rects randomly appear bottom left of mouse
   stroke(random(255), random(255), random(255))
   for (let x = 300; x < 1000; x++) {
     rect(mouseX - random(x), mouseY + random(x), mouseX - 300, mouseY - 420);
   }
+//rects randomly appear bottom right of mouse
   stroke(random(255), random(255), random(255))
   for (let x = 300; x < 1000; x++) {
     rect(mouseX + random(x), mouseY + random(x), mouseX - 300, mouseY - 420);
